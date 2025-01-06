b9123cdf34bb9d75e4f0549bdb51d17d3e3b2de4
HeliS29 <heli_shahgw@glorywebsdev.com>
2025-01-06 18:52:13 +0530

    allow multiple origin

--- main.py
+++ main.py
@@ -26,11 +26,10 @@ from fastapi.staticfiles import StaticFiles
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://192.168.3.19:49340"], 
-     # Allow all origins; modify as needed
+    allow_origins=["http://localhost:3000", "http://192.168.3.19:49340"],  # List of allowed origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
 )
 Base.metadata.create_all(bind=engine)
 
